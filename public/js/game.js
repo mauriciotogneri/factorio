2e0b3f56be0be9856dc9e85e980dd1e12092c5dd
Mauricio Togneri <mauricio.togneri@gmail.com>
2020-12-15 20:56:33 +0100

    Improved loop

--- public/js/game.js
+++ public/js/game.js
@@ -1,39 +1,51 @@
 class Game
 {
-    static map = null
-    static engine = null
-    static grid = null
-    static lastTimestamp = 0
+    static instance = null
 
-    static init()
+    map = null
+    engine = null
+    grid = null
+    lastTimestamp = 0
+
+    constructor()
     {
-        Game.map = new Map()
-        Game.map.load()
-        Game.engine = new Engine(Game.map)
+        this.map = new Map()
+        this.map.load()
+        this.engine = new Engine(this.map)
 
         const canvas = document.getElementById('canvas')
         canvas.width = document.body.clientWidth
         canvas.height = document.body.clientHeight
-        Game.grid = new Grid(canvas, Game.map)
-        Game.grid.render()
+        this.grid = new Grid(canvas, this.map)
+        this.grid.render()
+    }
 
+    loop(timestamp)
+    {
+        const start1 = performance.now()
+        const delta = (timestamp - this.lastTimestamp).toFixed(2)
+        this.engine.update(delta)
+        const end1 = performance.now()
+        console.log('update: ' + (end1 - start1).toFixed(2))
+
+        const start2 = performance.now()
+        this.grid.render()
+        const end2 = performance.now()
+        console.log('render: ' + (end2 - start2).toFixed(2))
+
+        this.lastTimestamp = timestamp
+
+        window.requestAnimationFrame(Game.step)
+    }
+
+    static init()
+    {
+        Game.instance = new Game()
         window.requestAnimationFrame(Game.step)
     }
 
     static step(timestamp)
     {
-        const start1 = performance.now()
-        const delta = (timestamp - Game.lastTimestamp).toFixed(2)
-        Game.engine.update(delta)
-        const end1 = performance.now()
-        console.log('update: ' + (end1 - start1).toFixed(2))
-
-        const start2 = performance.now()
-        Game.grid.render()
-        const end2 = performance.now()
-        console.log('render: ' + (end2 - start2).toFixed(2))
-
-        Game.lastTimestamp = timestamp
-        window.requestAnimationFrame(Game.step)
+        Game.instance.loop(timestamp)
     }
 }
